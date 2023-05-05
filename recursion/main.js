379911ad45e51230cb3e80459eca74a8304571b4
jared <jaredgold@proton.me>
2023-05-04 22:38:18 -0400

    set up workspace

---
+++ recursion/main.js
@@ -0,0 +1,8 @@
+/* Write a function called sumRange. It will take a number and return the sum of all 
+numbers from 1 up to the number passed in.
+
+Sample: sumRange(3) returns 6, since 1 + 2 + 3 = 6. */
+
+function sumRange(num) {
+  // call with 3, need 1 + 2 + 3 = 6
+}
