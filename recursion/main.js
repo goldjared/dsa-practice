7e81d33be63789d9dc8ec292b6d53a4e146ce0c6
jared <jaredgold@proton.me>
2023-05-10 22:53:46 -0400

    comment prev question, add power question

--- recursion/main.js
+++ recursion/main.js
@@ -3,16 +3,25 @@ numbers from 1 up to the number passed in.
 
 Sample: sumRange(3) returns 6, since 1 + 2 + 3 = 6. */
 
-function sumRange(num) {
-	if(num === 1) {
-		return 1;
-	}
-	return num + sumRange(num - 1);
-	/*
-	3 + 2
-	 + 1
-	*/
-}
+// function sumRange(num) {
+// 	if(num === 1) {
+// 		return 1;
+// 	}
+// 	return num + sumRange(num - 1);
+// 	/*
+// 	3 + 2
+// 	 + 1
+// 	*/
+// }
 
 
+
+/* Question 2: Power function
+Write a function called power which takes in a base and an exponent. If the exponent is 0, return 1.
+console.log(power(2, 4)); // 16
+console.log(power(2, 3)); // 8
+console.log(power(2, 2)); // 4 
+console.log(power(2, 1)); // 2
+console.log(power(2, 0)); // 1
+*/
 
