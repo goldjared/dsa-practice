e385e57f893750ef3351a44dcfd77af45aedf40a
jared <jaredgold@proton.me>
2023-05-11 16:10:11 -0400

    create recursive factorial func

--- recursion/main.js
+++ recursion/main.js
@@ -35,3 +35,17 @@ console.log(power(2, 0)); // 1
 
 //   */
 // }
+/*
+Question 3: Calculate factorial
+Write a function that returns the factorial of a number. As a quick refresher, 
+a factorial of a number is the result of that number multiplied by the number before it,
+ and the number before that number, and so on, until you reach 1. The factorial of 1 is just 1.
+
+Sample:
+factorial(5); // 5 * 4 * 3 * 2 * 1 === 120
+*/
+
+function factorial(num) {
+  if(num === 1) return 1;
+  return num * factorial(num - 1);
+}
