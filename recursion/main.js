192ff2a1d78374cd1f0b8d212ff1a20965b5e686
jared <jaredgold@proton.me>
2023-05-11 16:10:45 -0400

    comment code

--- recursion/main.js
+++ recursion/main.js
@@ -45,7 +45,11 @@ Sample:
 factorial(5); // 5 * 4 * 3 * 2 * 1 === 120
 */
 
-function factorial(num) {
-  if(num === 1) return 1;
-  return num * factorial(num - 1);
-}
+// function factorial(num) {
+//   if(num === 1) return 1;
+//   return num * factorial(num - 1);
+// }
+
+/*
+
+*/
