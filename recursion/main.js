a9e30a29c8119f5d124c6dc2d2cc4dcd595cbb59
jared <jaredgold@proton.me>
2023-05-14 12:37:15 -0400

    function sum array recursively

--- recursion/main.js
+++ recursion/main.js
@@ -202,3 +202,31 @@ console.log(SumSquares(l)); // 1 = 1
 l = [10,[[10],10],[10]] 
 console.log(SumSquares(l)); // 100 + 100 + 100 + 100 = 400
 */
+function SumSquares(array){
+	if(array.length === 0) return 0;
+	let total = 0;
+
+	for(let i = 0; i < array.length; i++){
+		if(Array.isArray(array[i])){
+			total += SumSquares(array[i]);
+		} else {
+			total += array[i] * array[i];
+		}
+		
+	}
+	return total;
+}
+
+
+// var l = [1,2,3]; 
+// console.log(sumSquares(l)); // 1 + 4 + 9 = 14
+
+// l = [[1,2],3]; 
+// console.log(sumSquares(l)); // 1 + 4 + 9 = 14
+
+// l = [[[[[[[[[1]]]]]]]]] 
+// console.log(sumSquares(l)); // 1 = 1
+// console.log(l === [])
+
+l = [10,[[10],10],[10]] 
+console.log(SumSquares(l)); // 100 + 100 + 100 + 100 = 400
