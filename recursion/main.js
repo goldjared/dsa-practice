f8dab069a0f0a00d7bdf1f7d154a615b7b589c35
jared <jaredgold@proton.me>
2023-05-14 21:14:59 -0400

    add fibonacci def

--- recursion/main.js
+++ recursion/main.js
@@ -253,5 +253,8 @@ of 8, this method should return the array [0, 1, 1, 2, 3, 5, 8, 13].
 Now write another method fibsRec which solves the same problem recursively. This can be
  done in just a couple of lines (or 1 if you’re crazy, but don’t consider either of 
   these lengths a requirement… just get it done).
+
+In mathematics, the Fibonacci sequence is a sequence
+in which each number is the sum of the two preceding ones. 
 */
 
