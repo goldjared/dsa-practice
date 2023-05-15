386cddde450d7b91a12b18767061d7d584215462
jared <jaredgold@proton.me>
2023-05-14 21:12:19 -0400

    add fibonacci questions

--- recursion/main.js
+++ recursion/main.js
@@ -245,3 +245,13 @@ return an empty array.
 // console.log(replicate(3, 5)) // [5, 5, 5]
 // // console.log(replicate(1, 69)) // [69]
 // // console.log(replicate(-2, 6)) // []
+/*
+Using iteration, write a function fibs which takes a number and returns an array 
+containing that many numbers from the fibonacci sequence. Using an example input 
+of 8, this method should return the array [0, 1, 1, 2, 3, 5, 8, 13].
+
+Now write another method fibsRec which solves the same problem recursively. This can be
+ done in just a couple of lines (or 1 if you’re crazy, but don’t consider either of 
+  these lengths a requirement… just get it done).
+*/
+
