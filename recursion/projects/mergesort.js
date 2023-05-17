77789204f46c74ac51d624dfb22e40666e86c36b
jared <jaredgold@proton.me>
2023-05-17 12:43:46 -0400

    set base case, get mid, left and right. create merge func

--- recursion/projects/mergesort.js
+++ recursion/projects/mergesort.js
@@ -7,9 +7,27 @@ again and again and can actually be delegated to someone else (e.g. that same me
 It may be helpful to check out the background videos again if 
 you don’t quite understand what should be going on.
 */
+/*
+pseudo:
+split arrays until 1 unit, if 1 unit, call merge with left, and the other side array.
+
+merge func (left, right)
+*/
 function mergeSort(array) {
-  
+  if(array.length <= 1) {
+    return array;
+  }
+  let mid = (array.length / 2);
+  let left = array.slice(0, mid);
+  let right = array.slice(mid, array.length);
+  return { left, right }
+  // mergeSort(array.length / 2)
+}
+
+function merge(l, r) {
+  return (l < r ? [l,r] : [r,l]);
 }
 
 // console.log(mergeSort([8,7,6,5,4,3,2,1]))
-console.log('wasabi')
+const xArr = [4,3,2,1]
+console.log(mergeSort(xArr))
