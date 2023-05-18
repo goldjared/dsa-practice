a8181581bdc4abb0b18603c959156508ed161174
jared <jaredgold@proton.me>
2023-05-18 18:25:07 -0400

    complete mergesort and merge funcs

--- recursion/projects/mergesort.js
+++ recursion/projects/mergesort.js
@@ -52,10 +52,61 @@ merge func (left, right)
 //     console.log('right array is: ', rightArray);
 //     array[index++] = rightArray.shift();
 //   }
-  
+
 //   console.log('** end of merge function ** array is: ', array);
 // }
 
 // // console.log(mergeSort([8,7,6,5,4,3,2,1]))
 // const xArr = [5, 4,3,2,1]
 // console.log(mergeSort(xArr))
+
+function mergeSort(array) {
+  /*
+  get array
+  split the left in half until 1 unit
+  split the right in half until 1 unit
+  then the merge sort will get called. with the left, and the right. it will sort them. 
+  */
+  if(array.length <= 1) {
+    return;
+  }
+  let mid = Math.ceil(array.length / 2);
+  let left = array.slice(0, mid)
+  let right = array.slice(mid, array.length)
+  mergeSort(left)
+  mergeSort(right)
+  merge(left, right, array);
+  // return { left, mid, right }
+  return array;
+}
+
+function merge(l,r,array) {
+console.log('left', l)
+console.log('right', r)
+console.log('array', array)
+  var index = 0;
+ 
+  while (l.length && r.length) {
+    console.log('array is: ', array);
+    if (r[0] < l[0]) {
+      array[index++] = r.shift();
+    } else {
+      array[index++] = l.shift();
+    }
+  }
+  
+  while (l.length) {
+    console.log('left array is: ', l);
+    array[index++] = l.shift();
+  }
+  
+  while (r.length) {
+    console.log('right array is: ', r);
+    array[index++] = r.shift();
+  }
+
+  console.log('** end of merge function ** array is: ', array);
+
+}
+
+console.log(mergeSort([5,4,3,2,1]))
