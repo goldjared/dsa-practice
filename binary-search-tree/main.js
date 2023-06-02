75a48f383646cf467d50f8d89bfba99384dd9b7b
jared <jaredgold@proton.me>
2023-06-02 17:58:57 -0400

    create tree class, move buildTree func within tree class

--- binary-search-tree/main.js
+++ binary-search-tree/main.js
@@ -5,20 +5,42 @@ class Node {
     this.right = null;
   }
 }
-
-
-function bST(array, start, end) {
-if(start > end) return null;
-let mid = parseInt(Math.floor(start + end) / 2);
-let node = new Node(array[mid]);
-node.left = bST(array, start,mid - 1);
-node.right = bST(array, mid + 1, end);
-return node;
+class Tree {
+  constructor(array, start, end) {
+    this.root = Tree.buildTree(array, start, end);
+  }
+  static buildTree(array, start, end) {
+    if(start > end) return null;
+    let mid = parseInt(Math.floor(start + end) / 2);
+    let node = new Node(array[mid]);
+    node.left = Tree.buildTree(array, start,mid - 1);
+    node.right = Tree.buildTree(array, mid + 1, end);
+    return node;
+  }
 }
+
+
+// function bST(array, start, end) {
+// if(start > end) return null;
+// let mid = parseInt(Math.floor(start + end) / 2);
+// let node = new Node(array[mid]);
+// node.left = bST(array, start,mid - 1);
+// node.right = bST(array, mid + 1, end);
+// return node;
+// }
 // mid 2
-const tester = [1,2,3,4,5,6,7]
+const tester = [1,2,3,4,5,6,7,8,9,11,12]
 const n = tester.length - 1;
-let newTree = bST(tester, 0, n);
+const myTree = new Tree(tester, 0, n);
+console.log(myTree);
+console.log(myTree.root.left.left.right);
+// let treeOne = new NodeTree(node.data, node.left, node.right)
+// let newTree = bST(tester, 0, n);
+// console.log(newTree, 'newTreeb4class');
+// let treeOne = new NodeTree(newTree.data, newTree.left, newTree.right)
+// console.log(newTree.left, 'newtree');
+// console.log(treeOne, 'treeuno');
+
 
 const prettyPrint = (node, prefix = "", isLeft = true) => {
   if (node === null) {
@@ -32,4 +54,5 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
     prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
   }
 };
-prettyPrint(newTree);
+
+// prettyPrint(treeOne);
