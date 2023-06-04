cee686c8d9e6e867d9ecfb300096d132716b15d2
jared <jaredgold@proton.me>
2023-06-03 21:48:57 -0400

    implement find method in tree class

--- binary-search-tree/main.js
+++ binary-search-tree/main.js
@@ -21,6 +21,24 @@ class Tree {
   view() {
     console.log(this);
   }
+  find(val, currentNode) {
+
+    if (currentNode === undefined) {
+      currentNode = this.root;
+    } 
+    if(currentNode === null) {
+      return false;
+    }
+    if(val === currentNode.data) {
+      return currentNode;
+    } else if(val < currentNode.data) {
+      return this.find(val, currentNode.left)
+    } else if(val > currentNode.data) {
+      return this.find(val, currentNode.right)
+    } else {
+      return false;
+    }
+  }
 
   insert(val, currentNode) {
     if (currentNode === undefined) {
@@ -40,16 +58,33 @@ class Tree {
       return this.insert(val, currentNode);
     }
   }
+  delete(val, currentNode) {
+    /*say i wanat to delete 4, which has a right child of 5.
+    first ill have to traverse down and find 4, if its not there error.
+    ok i found 4, if 4 . left and right = null, i delete node 4, also got to set the previous nodes
+    next node to null. so wahtever val was connected to that node, set the connection to null
+
+    but if left and right != null, this mean there r nodes after the delete val.
+    in this case, ill have to set the connect above delete val, link it to the val delete was connected
+
+
+    */
+console.log('HERE!!!!!!!!!!', this.find(val));
+  }
 }
-const tester = [4, 5, 6];
+const tester = [4, 5, 6,8,9];
 const n = tester.length - 1;
 const myTree = new Tree(tester);
-// console.log(myTree);
+console.log(myTree);
+myTree.delete(9)
 // myTree.view();
-myTree.insert(7);
-setTimeout(() => {
-  console.log(myTree.view());
-}, 3000);
+// myTree.insert(7);
+// myTree.insert(9);
+// myTree.insert(1);
+// myTree.insert(-20);
+// setTimeout(() => {
+//   console.log(myTree.view());
+// }, 3000);
 
 const prettyPrint = (node, prefix = "", isLeft = true) => {
   if (node === null) {
@@ -64,4 +99,4 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
   }
 };
 
-// prettyPrint(myTree);
+prettyPrint(myTree.root);
