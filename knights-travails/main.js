d5f16f5c7d4a82c0a60e8dbd1cc3e376ba6a2258
jared <jaredgold@proton.me>
2023-06-20 21:26:02 -0400

    format with prettier

--- knights-travails/main.js
+++ knights-travails/main.js
@@ -1,47 +1,52 @@
 function game() {
   function board(row, boardArray = []) {
-    if(row === 9) {
+    if (row === 9) {
       return boardArray;
     }
-    for(let i = 1; i < 9; i++) {
-      boardArray.push([row, i])
+    for (let i = 1; i < 9; i++) {
+      boardArray.push([row, i]);
     }
     return board(row + 1, boardArray);
-
   }
 
   function knight() {
     let move = (currentPos, targetPos) => {
-let colValue = 0;
-let rowValue = 0;
-if(currentPos[0] < targetPos[0]) {
- colValue = targetPos[0] - currentPos[0];
-} else {
-  colValue = currentPos[0] - targetPos[0];
-}
-if(currentPos[1] < targetPos[1]) {
- rowValue = targetPos[1] - currentPos[1];
-} else {
-  rowValue = currentPos[1] - targetPos[1];
-}
-console.log(colValue, rowValue, 'col, row');
-if((colValue < 1 || colValue > 2) || (rowValue < 1 || rowValue > 2) || (colValue != rowValue)) {
-  return false;
-} else {
-  currentPos = targetPos;
-  return true;
-}
-}
+      let colValue = 0;
+      let rowValue = 0;
+      if (currentPos[0] < targetPos[0]) {
+        colValue = targetPos[0] - currentPos[0];
+      } else {
+        colValue = currentPos[0] - targetPos[0];
+      }
+      if (currentPos[1] < targetPos[1]) {
+        rowValue = targetPos[1] - currentPos[1];
+      } else {
+        rowValue = currentPos[1] - targetPos[1];
+      }
+      console.log(colValue, rowValue, "col, row");
+      if (
+        colValue < 1 ||
+        colValue > 2 ||
+        rowValue < 1 ||
+        rowValue > 2 ||
+        colValue != rowValue
+      ) {
+        return false;
+      } else {
+        currentPos = targetPos;
+        return true;
+      }
+    };
 
-return { move }
+    return { move };
   }
-  return { board, knight }
+  return { board, knight };
 }
-let chessBoard = game().board(1)
-let myKnight = game().knight()
+let chessBoard = game().board(1);
+let myKnight = game().knight();
 console.log(chessBoard.length);
 // console.log(myKnight);
-console.log(myKnight.move([8,6], [3,6]));
+console.log(myKnight.move([8, 6], [3, 6]));
 
 /*
   knightMOve(current, target)
